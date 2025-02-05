e8a1a93e33d66af4a2f2700b270147eaa00c1c46
VasareM <vasare.milkovaite@mif.stud.vu.lt>
2025-02-05 13:27:31 +0200

    vektoriai atnaujinti

---
+++ mano_lib.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::string;
+using std::vector;
+using std::endl;
+
+struct Stud {
+    string var="A";
+    string pav="A";
+    vector<float> paz;
+    int egz;
+};
